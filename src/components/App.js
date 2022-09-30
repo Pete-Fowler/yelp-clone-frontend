1cb04e63ddc2c29e8b4ee61cef41beb24cf8c2b8
ChaseWelsh <fseusb@gmail.com>
2022-09-30 11:25:46 -0600

    add routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,18 @@
+import { Route, Routes } from 'react-router-dom';
 import '../App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="App">
+      <Header/>
+      <Routes>
+        <Route path="/home" element={<div/>}/>
+        <Route path="/login" element={<div/>}/>
 
+        <Route path="/businesses" element={<div/>}/>
+        <Route path="/business" element={<div/>}/>
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <nav>
+      <Link to="/">Home</Link>
+      <Link to="/businesses">Businesses</Link>
+      <Link to="/login">Login</Link>
+    </nav>
+  )
+}
+
+export default Header
