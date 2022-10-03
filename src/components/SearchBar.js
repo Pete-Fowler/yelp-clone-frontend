f3038f16d013655916f3b9a64d784b8d33ecebfe
Pete Fowler <fowler.pete@gmail.com>
2022-10-03 11:32:13 -0600

    Add style to search bar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import styles from './SearchBar.module.css';
+import searchIcon from '../images/searchIcon.svg';
 
 export default function SearchBar () {
 
   return <div className={styles.searchBar}>
-    <form>
-
-      <input type='text' placeholder="tacos, cheap dinner, Max's"></input>
-      <input type='text' placeholder="Denver, CO 80203"></input>
+    <form className={styles.form}>
+      <input type='text' className={styles.input} placeholder="tacos, cheap dinner, Max's"></input>
+      <input type='text' className={styles.input} placeholder="Denver, CO 80203"></input>
+      <button className={styles.searchBtn} type='submit'><img src={searchIcon} className={styles.searchIcon} alt='Search icon' /></button>
     </form>
   </div>
 }
