b5ddd762c9b86afb6954aee9f0367a2ee725cb70
Pete Fowler <fowler.pete@gmail.com>
2022-10-03 10:16:59 -0600

    Add yelp logo to nav and structure

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,7 @@
+import React from 'react';
+import logo from '../images/yelpLogo.svg';
+import styles from './Header.module.css';
+import SearchBar from './SearchBar';
 import { Link } from "react-router-dom";
 
 function Header({isLoggedIn, logOut}) {
@@ -7,7 +11,9 @@ function Header({isLoggedIn, logOut}) {
   }
 
   return (
-    <nav className="centered row">
+    <nav className={styles.nav}>
+      <img src={logo} alt='Yelp logo' />
+      <SearchBar />
       <Link to="/">Home</Link>
       <Link to="/businesses">Businesses</Link>
       {userArea}
