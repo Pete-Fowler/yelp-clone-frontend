ffee1425ff597ed73b15f167ac2690a33407acbc
Tim <dalytimdaly@gmail.com>
2022-10-03 11:41:40 -0600

    hover effect for star rating

--- src/components/StarRating.js
+++ src/components/StarRating.js
@@ -3,6 +3,7 @@ import "../StarRating.css";
 
 function StarRating() {
     const [rating, setRating] = useState(0)
+    const [hover, setHover] = useState(0)
 
     return (
     <div className="star-rating">
@@ -12,8 +13,10 @@ function StarRating() {
                 <button
                 type="button"
                 key={index}
-                className={index <= rating ? "on" : "off"}
+                className={index <= ((rating && hover) || hover ) ? "on" : "off"}
                 onClick={() => setRating(index)}
+                onMouseEnter={() => setHover(index)}
+                onMouseLeave={() => setHover(rating)}
                 >    
             <span className="star">&#9733;</span>
             </button>             
