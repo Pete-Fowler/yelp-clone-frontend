685cffed742a4b8a3bfdaf25427e198db9956d0c
Pete Fowler <fowler.pete@gmail.com>
2022-10-03 11:41:05 -0600

    Add handleSubmit skeleton

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -9,8 +9,12 @@ export default function SearchBar () {
     setSearchTerm(e.target.value);
   }
 
+  function handleSubmit(e) {
+    
+  }
+
   return <div className={styles.searchBar}>
-    <form className={styles.form}>
+    <form className={styles.form} onSubmit={handleSubmit}>
       <input type='text' className={styles.input} value={searchTerm} onChange={handleChange}placeholder="tacos, cheap dinner, Max's"></input>
       <input type='text' className={styles.input} placeholder="Denver, CO 80203"></input>
       <button className={styles.searchBtn} type='submit'><img src={searchIcon} className={styles.searchIcon} alt='Search icon' /></button>
