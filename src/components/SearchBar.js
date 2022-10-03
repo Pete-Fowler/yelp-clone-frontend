ddafb41b6db3786740645be977fe2d3a0229f3ca
Pete Fowler <fowler.pete@gmail.com>
2022-10-03 11:04:31 -0600

    Style inputs

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,6 +4,10 @@ import styles from './SearchBar.module.css';
 export default function SearchBar () {
 
   return <div className={styles.searchBar}>
+    <form>
 
+      <input type='text' placeholder="tacos, cheap dinner, Max's"></input>
+      <input type='text' placeholder="Denver, CO 80203"></input>
+    </form>
   </div>
 }
