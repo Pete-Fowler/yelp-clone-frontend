ed900abba9385096b404ab151d84a957a79c042c
Pete Fowler <fowler.pete@gmail.com>
2022-10-03 11:37:18 -0600

    Add state for controlled input

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './SearchBar.module.css';
 import searchIcon from '../images/searchIcon.svg';
 
 export default function SearchBar () {
+  const [ searchTerm, setSearchTerm ] = useState("") 
+
+  function handleChange(e) {
+    setSearchTerm(e.target.value);
+  }
 
   return <div className={styles.searchBar}>
     <form className={styles.form}>
-      <input type='text' className={styles.input} placeholder="tacos, cheap dinner, Max's"></input>
+      <input type='text' className={styles.input} value={searchTerm} onChange={handleChange}placeholder="tacos, cheap dinner, Max's"></input>
       <input type='text' className={styles.input} placeholder="Denver, CO 80203"></input>
       <button className={styles.searchBtn} type='submit'><img src={searchIcon} className={styles.searchIcon} alt='Search icon' /></button>
     </form>
