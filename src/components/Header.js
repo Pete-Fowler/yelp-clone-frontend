717f2a01551a4d8b08e73afbb231ca9ba6c828b4
Pete Fowler <fowler.pete@gmail.com>
2022-10-04 15:28:40 -0600

    Render header based on route

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,29 @@
-import React from 'react';
-import logo from '../images/yelpLogoWhite.svg';
+import React, { useState, useEffect } from 'react';
+import logoWhite from '../images/yelpLogoWhite.svg';
+import logoColor from '../images/yelpLogo.svg';
 import styles from './Header.module.css';
 import SearchBar from './SearchBar';
 import { Link, useNavigate } from "react-router-dom";
 
-function Header({ isLoggedIn, logOut, handleSearch }) {
+function Header({ isLoggedIn, logOut }) {
+  const [ onHome, setOnHome ] = useState(true);
+
+  useEffect(() => {
+    if (window.location.pathname === "/")
+      setOnHome(true);
+    else 
+      setOnHome(false);
+  }, [window.location.pathname])
+
+  const headerClass = onHome ? styles.absolute : styles.sticky;
+  const logoSrc = onHome ? logoWhite : logoColor;
+  const loginClass = onHome ? styles.headerBtn : styles.stickyBtn;
+
   const navigate = useNavigate()
 
   let userArea = (
     <div className={"row " + styles.userArea}>
-      <button className={'login ' + styles.headerBtn} onClick={()=>{navigate("/login")}}>Log In</button>
+      <button className={'login ' + loginClass} onClick={()=>{navigate("/login")}}>Log In</button>
       <button className={'red ' + styles.signUp + ' ' + styles.headerBtn} onClick={()=>{navigate("/signup")}}>Sign Up</button>
     </div>
   )
@@ -17,10 +31,12 @@ function Header({ isLoggedIn, logOut, handleSearch }) {
     userArea = ( <button onClick={logOut}> Log Out </button> )
   }
 
+
+
   return (
-    <nav className={styles.nav}>
-        <Link to="/"><img className={styles.logo} src={logo} alt='Yelp logo' /></Link>
-        <SearchBar handleSearch={handleSearch}/>
+    <nav className={styles.nav + ' ' + headerClass}>
+        <Link to="/"><img className={styles.logo} src={logoSrc} alt='Yelp logo' /></Link>
+        <SearchBar />
       {userArea}
     </nav>
   )
