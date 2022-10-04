7c582d66a7762e3bb89ced50170cda109fcf0c2a
ChaseWelsh <fseusb@gmail.com>
2022-10-04 13:45:44 -0600

    add phone number and website

--- src/components/BusinessDetails.js
+++ src/components/BusinessDetails.js
@@ -11,6 +11,8 @@ function BusinessDetails({isLoggedIn}) {
   const [imgUrl, setImgUrl] = useState("")
   const [price, setPrice] = useState("")
   const [reviews, setReviews] = useState([])
+  const [phoneNumber, setPhoneNumber] = useState("")
+  const [website, setWebsite] = useState("")
 
   const navigate = useNavigate()
   const { id } = useParams();
@@ -26,6 +28,8 @@ function BusinessDetails({isLoggedIn}) {
       setImgUrl(data.image_url)
       setPrice(data.price)
       setReviews(data.reviews)
+      setPhoneNumber(data.phone_number)
+      setWebsite(data.website)
     })
   }, [id])
 
@@ -85,13 +89,13 @@ function BusinessDetails({isLoggedIn}) {
           </div>
           <div className={`col outlineBox`}>
             <div className="row">
-              <a href="#">http://www.businesswebsite.com</a>
+              <a className="centered" href="#">{website}</a>
               <div style={{flex:1}}/>
               <svg width="24" height="24"><path d="M20.47 3.07a.5.5 0 01.53.46v6a.5.5 0 01-.39.49.58.58 0 01-.19 0 .47.47 0 01-.35-.15L17.8 7.6l-5 5a1 1 0 01-1.41 0 1 1 0 010-1.41l5-5-2.27-2.27a.5.5 0 01.35-.85h6zM20 21H4a1 1 0 01-1-1V4a1 1 0 011-1h6a1 1 0 010 2H5v14h14v-5a1 1 0 012 0v6a1 1 0 01-1 1z"></path></svg>
             </div>
             <hr/>
             <div className="row">
-              (555) 123-4567
+              <span className="centered">{phoneNumber}</span>
               <div style={{flex:1}}/>
               <svg width="24" height="24"><path d="M13.59 23.07A7 7 0 018.64 21L3 15.36a7 7 0 010-9.9l1.39-1.41a1 1 0 011.42 0l5 5a1 1 0 010 1.41 2.001 2.001 0 002.83 2.83 1 1 0 011.41 0l4.95 5a1 1 0 010 1.42L18.54 21a7 7 0 01-4.95 2.07zM5.1 6.17l-.71.71a5 5 0 000 7.07l5.66 5.66a5 5 0 007.07 0l.71-.71-3.63-3.63a4 4 0 01-4.86-.61 4 4 0 01-.61-4.86L5.1 6.17zm12.78 5.95a1 1 0 01-1-1 4 4 0 00-4-4 1 1 0 010-2 6 6 0 016 6 1 1 0 01-1 1zm4.19 0a1 1 0 01-1-1 8.19 8.19 0 00-8.19-8.19 1 1 0 010-2c5.625.006 10.184 4.565 10.19 10.19a1 1 0 01-1 1z"></path></svg>
             </div>
