eeac8e67bf64696961a8d00821ade38af952a0b7
Tim <dalytimdaly@gmail.com>
2022-10-04 17:12:40 -0600

    user stars functional

--- src/components/StarRating.js
+++ src/components/StarRating.js
@@ -5,22 +5,27 @@ function StarRating({ id, name, type, address, reviews, price, image_url }) {
 
   const starTotal = reviews.reduce((last, current) => {
     return last + current.star_rating;
-  }, reviews[0].star_rating);
+  }, reviews.star_rating);
+
+  let index = 0
 
   const starAverage = Math.round(starTotal / reviews.length * 10) / 10;
     
   const displayStars = Array.from(String(starAverage), Number);
 
   const results = displayStars.map((star) => {
-    return <span className='star-average'>&#9733;</span>
+    return <span key={index ++} className='star-average'>&#9733;</span>
   })
 
     
    
 
     return (   
-   <div>{results}</div>
-    )
+      
+      <div>{results}<button onClick={() => console.log(reviews)}></button></div>
+      
+    
+      )
   
 }
 
