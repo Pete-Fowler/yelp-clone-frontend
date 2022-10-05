85393df3367bb3073254d4ab73fe377bec8dc54d
Pete Fowler <fowler.pete@gmail.com>
2022-10-05 11:50:04 -0600

    Conditionally render stars in business detail

--- src/components/BusinessDetails.js
+++ src/components/BusinessDetails.js
@@ -28,7 +28,7 @@ function BusinessDetails({isLoggedIn}) {
       <div id={style.photoHeader} style={{ background: `linear-gradient(to bottom, transparent, rgba(0,0,0,0.75)), url(${bizObject.image_url})`, position:"relative"}}>
         <div className="col" style={{bottom:0, left:0, position:"absolute"}}>
           <h1>{bizObject.name}</h1>
-          <span><StarRating reviews={bizObject.reviews}/></span>
+          <span>{bizObject.reviews.length !== 0 ? <StarRating reviews={bizObject.reviews}/> : null}</span>
           <span> {bizObject.price} • {bizObject.business_type} </span>
         </div>
       </div>
