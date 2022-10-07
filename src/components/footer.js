d196e7d419d0b7ddfc30dd449ebffec23b3e6326
Pete Fowler <fowler.pete@gmail.com>
2022-10-07 17:08:37 -0600

    Prevent footer links from jumping to top of page

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,49 +6,48 @@ function Footer() {
     <div className="row" id="footer">
       <div className="col">
         <strong>About</strong>
-        <a href="#">About Yelp</a>
-        <a href="#">Careers</a>
-        <a href="#">Press</a>
-        <a href="#">Investor Relations</a>
-        <a href="#">Trust & Safety</a>
-        <a href="#">Content Guidelines</a>
-        <a href="#">Accessibility Statement</a>
-        <a href="#">Terms of Service</a>
-        <a href="#">Privacy Policy</a>
-        <a href="#">Ad Choices</a>
-        <a href="#">Manage Cookies</a>
+        <a href="#/">About Yelp</a>
+        <a href="#/">Careers</a>
+        <a href="#/">Press</a>
+        <a href="#/">Investor Relations</a>
+        <a href="#/">Trust & Safety</a>
+        <a href="#/">Content Guidelines</a>
+        <a href="#/">Accessibility Statement</a>
+        <a href="#/">Terms of Service</a>
+        <a href="#/">Privacy Policy</a>
+        <a href="#/">Ad Choices</a>
+        <a href="#/">Manage Cookies</a>
 
       </div>
       <div className="col">
         <strong>Discover</strong>
-        <a href="#">Yelp Project Cost Guides</a>
-        <a href="#">Collections</a>
-        <a href="#">Talk</a>
-        <a href="#">Events</a>
-        <a href="#">Yelp Blog</a>
-        <a href="#">Support</a>
-        <a href="#">Yelp Mobile</a>
-        <a href="#">Developers</a>
-        <a href="#">RSS</a>
+        <a href="#/">Yelp Project Cost Guides</a>
+        <a href="#/">Collections</a>
+        <a href="#/">Talk</a>
+        <a href="#/">Events</a>
+        <a href="#/">Yelp Blog</a>
+        <a href="#/">Support</a>
+        <a href="#/">Yelp Mobile</a>
+        <a href="#/">Developers</a>
+        <a href="#/">RSS</a>
       </div>
       <div className="col">
         <strong>Yelp for Business</strong>
-        <a href="#">Claim your Business Page</a>
-        <a href="#">Advertise on Yelp</a>
-        <a href="#">Yelp for Restaurant Owners</a>
-        <a href="#">Table Management</a>
-        <a href="#">Business Success Stories</a>
-        <a href="#">Business Support</a>
-        <a href="#">Yelp Blog for Business</a>
+        <a href="#/">Claim your Business Page</a>
+        <a href="#/">Advertise on Yelp</a>
+        <a href="#/">Yelp for Restaurant Owners</a>
+        <a href="#/">Table Management</a>
+        <a href="#/">Business Success Stories</a>
+        <a href="#/">Business Support</a>
+        <a href="#/">Yelp Blog for Business</a>
       </div>
       <div className="col">
         <strong>Languages</strong>
-        <a href="#">English</a>
+        <a href="#/">English</a>
         <br></br>
         <strong>Countries</strong>
-        <a href="#">United States</a>
+        <a href="#/">United States</a>
       </div>
-      {/* <div style={{flex:"1"}}/> */}
     </div>
   );
 }
