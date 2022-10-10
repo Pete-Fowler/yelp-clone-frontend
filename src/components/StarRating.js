ed196ee7e7d2f2cda5304a829a3fcc006cddb664
Pete Fowler <fowler.pete@gmail.com>
2022-10-10 14:56:01 -0600

    Refactor starTotal in starRating

--- src/components/StarRating.js
+++ src/components/StarRating.js
@@ -6,58 +6,49 @@ function StarRating({ reviews, displayDetails = true }) {
     return <div className={style.starComponent}>(0 reviews)</div>
   }
 
-  let starTotal;
+  let starTotal = 0;
   let starAverage;
   
+  reviews.forEach(review => starTotal += review.star_rating);
+  starAverage = (Math.round(starTotal / reviews.length * 10) / 10);
 
-  if(reviews.length > 1) {
-    starTotal = reviews.reduce((last, current) => {
-      return last + current.star_rating;
-    }, reviews[0].star_rating);
-    starAverage = (Math.round(starTotal / reviews.length * 10) / 10);
+  const fullStars = Math.floor(starAverage);
+
+  const starArr = [];
+
+  for(let i = 1; i <= fullStars; i++)
+  {
+    starArr.push(1);
   }
-  else {
-    starAverage = reviews[0].star_rating;
+
+  if(starAverage < 5) {
+  const partialStar = starAverage - fullStars;
+  starArr.push(partialStar);
+
+  const emptyStars = 5 - starArr.length;
+    for(let i=1; i<=emptyStars; i++) {
+    starArr.push(0);
+    }
   }
+
+  const stars = starArr.map((val, i) => {
+    return <div key={i} className={style.starBox} style={{background: `linear-gradient(90deg, #ff643d, #ff643d ${val * 100}%, #bbbac0 ${val * 100}%)`}}>★</div>
+  })
+
   
 
-    const fullStars = Math.floor(starAverage);
+  const details = displayDetails ? <div className={style.data}>
+    <b className={style.average}>{starAverage > 5 ? 5 : starAverage}</b> ({reviews.length} reviews)</div>
+    : null;
 
-    const starArr = [];
-
-    for(let i = 1; i <= fullStars; i++)
-    {
-      starArr.push(1);
-    }
-
-    if(starAverage < 5) {
-    const partialStar = starAverage - fullStars;
-    starArr.push(partialStar);
-
-    const emptyStars = 5 - starArr.length;
-      for(let i=1; i<=emptyStars; i++) {
-      starArr.push(0);
-      }
-    }
-
-    const stars = starArr.map((val, i) => {
-      return <div key={i} className={style.starBox} style={{background: `linear-gradient(90deg, #ff643d, #ff643d ${val * 100}%, #bbbac0 ${val * 100}%)`}}>★</div>
-    })
-
-    
-
-    const details = displayDetails ? <div className={style.data}>
-      <b className={style.average}>{starAverage > 5 ? 5 : starAverage}</b> ({reviews.length} reviews)</div>
-      : null;
-
-    return (
-    <div className={style.starComponent}>
-      <div className={style.starsBox} >
-        {stars}
-      </div>
-      {details}
+  return (
+  <div className={style.starComponent}>
+    <div className={style.starsBox} >
+      {stars}
     </div>
-    )
+    {details}
+  </div>
+  )
 
 }
 
